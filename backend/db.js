e3ebbba012958a896d0bf875acadc76f7ee33114
Sandile Kitayi The Developer <ksandile127@gmail.com>
2024-11-11 13:43:26 +0200

    Added server dependencies

--- backend/db.js
+++ backend/db.js
@@ -3,15 +3,15 @@ const { Pool } = require('pg');
 
 // Configure your PostgreSQL database connection
 const pool = new Pool({
-  user: 'your_username',
-  host: 'localhost',
-  database: 'your_database_name',
-  password: 'your_password',
-  port: 5432, // Default PostgreSQL port
+  user: 'postgres',            // Your PostgreSQL username
+  host: 'localhost',           // Your PostgreSQL server
+  database: 'payroll_management', // Replace with your database name
+  password: 'new_password',   // Replace with your password
+  port: 5432,                  // Default PostgreSQL port
 });
 
 pool.on('connect', () => {
-  console.log('Connected to the database');
+  console.log('Connected to the PostgreSQL database');
 });
 
 module.exports = pool;
